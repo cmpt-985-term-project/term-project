ac622381ab6c952ea78dc9ba5355caf359d87641
Geoffrey Mantel <gmantel@sfu.ca>
2022-11-24 11:32:53 -0800

    Trying frequency-based positional encoding for a bit...

--- nsff_exp/models/fused_nerf.py
+++ nsff_exp/models/fused_nerf.py
@@ -32,13 +32,17 @@ class FusedDensityMLP(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(FusedDensityMLP, self).__init__()
 
-        encoding_config = json.loads('''
-            {"otype":"Grid", "type":"Hash", "n_levels":16, "n_features_per_level":2, "log2_hashmap_size":19,
-             "base_resolution":16, "per_level_scale": 2.0, "interpolation": "Linear"}''')
+        # Hash encoding -- needs more investigation...
+        #encoding_config = json.loads('''
+        #    {"otype":"Grid", "type":"Hash", "n_levels":16, "n_features_per_level":2, "log2_hashmap_size":19,
+        #     "base_resolution":16, "per_level_scale": 2.0, "interpolation": "Linear"}''')
+
+        # Frequency encoding
+        encoding_config = json.loads('{"otype":"Frequency", "n_frequencies":10}')
 
         network_config = json.loads('''
             {"otype":"FullyFusedMLP", "activation":"ReLU", "output_activation":"None", "n_neurons":64,
-             "n_hidden_layers":1, "feedback_alignment":false}''')
+             "n_hidden_layers":3, "feedback_alignment":false}''')
 
         self.network = tcnn.NetworkWithInputEncoding(n_input_dims=in_channels, n_output_dims=out_channels,
                                                      encoding_config=encoding_config, network_config=network_config)
