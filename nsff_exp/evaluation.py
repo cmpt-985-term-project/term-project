FP16 Precision Fixes, AMP implementation, and more (#21)
* I think I fixed the bug - trying out fix on CutlassMLP

* Switching encoder to original PyTorch version

* Fixed missing RELU output on part1 of density network

* Fixed wrong number of hidden layers in part 1 of cutlass model

* A few small changes

* Splitting out dimensions correctly

* Unnecessary cast to FP16

* Set boto3 requirements for now

* Temporarily going back to FP32 - I forgot I had the loss scaling issue

* Switching to hash encoding for position and spherical harmonic encoding for viewing angle

* Adding support for automated mixed precision

* AMP with float16 instead of bfloat16

* I think this is it. Changing eps values from 1e-10 to 1e-7 for FP16 stability

* Fixed a couple overflow bugs and a NDC-z clamping issue

* Going back to original Tiny-CUDA-nn

* Going back to positional encoding for FusedMLP. Should make this configurable

* Removed PyTorch positional encoder from CutlassMLP. This was only ever intended as a temporary test

* Fixed the missing ReLU activation in 'part1' of the FusedDensityMLP implementation. Can't believe I missed that!

nsff_exp/evaluation.py
@@ -152,7 +152,7 @@ def calculate_psnr(img1, img2, mask):
     img2 = img2.astype(np.float64)
     mask = mask.astype(np.float64)
 
-    num_valid = np.sum(mask) + 1e-8
+    num_valid = np.sum(mask) + 1e-7
 
     mse = np.sum((img1 - img2)**2 * mask) / num_valid
     
@@ -171,7 +171,7 @@ def calculate_ssim(img1, img2, mask):
         raise ValueError('Input images must have the same dimensions.')
 
     _, ssim_map = structural_similarity(img1, img2, multichannel=True, full=True)
-    num_valid = np.sum(mask) + 1e-8
+    num_valid = np.sum(mask) + 1e-7
 
     return np.sum(ssim_map * mask) / num_valid
 

nsff_exp/models/fused_nerf.py
@@ -35,10 +35,13 @@ class FusedDensityMLP(nn.Module):
         self.W = 128
 
         encoding_config = json.loads(f'{{"otype":"Frequency", "n_frequencies":{degrees}}}')
+        #encoding_config = json.loads(f'''
+        #    {{"otype":"Grid", "type":"Hash", "n_levels":16, "n_features_per_level":2, "log2_hashmap_size": 19,
+        #      "base_resolution": 16, "per_level_scale": 2.0, "interpolation": "Linear"}}''')
         self.position_encoder = tcnn.Encoding(n_input_dims=in_channels, encoding_config=encoding_config)
 
         network_config1 = json.loads(f'''
-            {{"otype":"FullyFusedMLP", "activation":"ReLU", "output_activation":"None", "n_neurons":{self.W},
+            {{"otype":"FullyFusedMLP", "activation":"ReLU", "output_activation":"ReLU", "n_neurons":{self.W},
               "n_hidden_layers":2, "feedback_alignment":false}}''')
         self.model_part1 = tcnn.Network(n_input_dims=self.position_encoder.n_output_dims, n_output_dims=self.W, network_config=network_config1)
 
@@ -59,9 +62,8 @@ class FusedColorMLP(nn.Module):
         super(FusedColorMLP, self).__init__()
         self.W = 128
 
-        # For consistency with original paper, we will use the position encoder on the viewing angle,
-        # even though a spherical harmonic encoder makes more sense.
-        encoding_config = json.loads(f'{{"otype":"Frequency", "n_frequencies":{degrees}}}')
+        #encoding_config = json.loads(f'{{"otype":"Frequency", "n_frequencies":{degrees}}}')
+        encoding_config = json.loads(f'{{"otype":"SphericalHarmonics", "degree":4}}')
         self.view_encoder = tcnn.Encoding(n_input_dims=3, encoding_config=encoding_config)
 
         network_config = json.loads(f'''
@@ -81,28 +83,23 @@ class FusedDynamicNeRF(nn.Module):
         super(FusedDynamicNeRF, self).__init__()
         self.W = 128
 
-        # 24 channels = scene flow (2 x 3-dim) + disocclusion weights (2 x 1-dim) + density and feature vector (128-dim)
-        self.density_mlp = FusedDensityMLP(in_channels=4, out_channels=self.W + 8)
+        # 24 channels = scene flow (2 x 3-dim) + disocclusion weights (2 x 1-dim) + 1-dim density + 128-dim feature vector
+        self.density_mlp = FusedDensityMLP(in_channels=4, out_channels=self.W + 6 + 2 + 1 )
         self.color_mlp = FusedColorMLP()
 
     @nvtx.annotate("Fused Dynamic NeRF forward")
     def forward(self, x):
-        # Fused NeRF is 16-bit internally
-        x = x.to(torch.float16)
-
         # 4 input channels, 3 view channels
         input_position, input_view = x.split([4, 3], dim=-1)
         x = self.density_mlp(input_position)
 
-        # 2 x 3-dim scene flow, 2 x 1-dim disocclusion blend, 128-dim feature vector
-        scene_flow, disocclusion_blend, feature_vector = torch.split(x, [6, 2, self.W], dim=-1)
+        # 2 x 3-dim scene flow, 2 x 1-dim disocclusion blend, 1 density value, and 128-dim feature vector
+        scene_flow, disocclusion_blend, density, feature_vector = torch.split(x, [6, 2, 1, self.W], dim=-1)
 
-        scene_flow = torch.tanh(scene_flow.to(torch.float32))
-        disocclusion_blend = torch.sigmoid(disocclusion_blend.to(torch.float32))
-        density = feature_vector[:, 0:1].to(torch.float32)
+        scene_flow = torch.tanh(scene_flow)
+        disocclusion_blend = torch.sigmoid(disocclusion_blend)
 
         rgb = self.color_mlp(torch.cat([input_view, feature_vector], dim=-1))
-        rgb = rgb.to(torch.float32)
 
         return torch.cat([rgb, density, scene_flow, disocclusion_blend], dim=-1)
 
@@ -112,26 +109,21 @@ class FusedStaticNeRF(nn.Module):
         super(FusedStaticNeRF, self).__init__()
         self.W = 128
 
-        # 17 channels = static/dynamic blending weight (1-dim) + density and feature vector (128-dim)
-        self.density_mlp = FusedDensityMLP(in_channels=3, out_channels=self.W+1)
+        # 17 channels = static/dynamic blending weight (1-dim) + 1-dim density and 128-dim feature vector
+        self.density_mlp = FusedDensityMLP(in_channels=3, out_channels=self.W + 1 + 1)
         self.color_mlp = FusedColorMLP()
 
     @nvtx.annotate("Fused Static NeRF forward")
     def forward(self, x):
-        # Fused NeRF is 16-bit internally
-        x = x.to(torch.float16)
-
         # 3 input channels, 3 view channels
         input_position, input_view = x.split([3, 3], dim=-1)
         x = self.density_mlp(input_position)
 
         # 1-dim blending weight, 128-dim feature vector
-        blending, feature_vector = x.split([1, self.W], dim=-1)
+        blending, density, feature_vector = x.split([1, 1, self.W], dim=-1)
 
-        blending = torch.sigmoid(blending.to(torch.float32))
-        density = feature_vector[:, 0:1].to(torch.float32)
+        blending = torch.sigmoid(blending)
 
         rgb = self.color_mlp(torch.cat([input_view, feature_vector], dim=-1))
-        rgb = rgb.to(torch.float32)
 
         return torch.cat([rgb, density, blending], dim=-1)

nsff_exp/render_utils.py
@@ -176,7 +176,7 @@ def render_slowmo_bt(disps, render_poses, bt_poses,
             alpha_final = alpha_1_final + alpha_2_fianl
 
             # final_depth += T_i * (alpha_final) * z_vals[..., j]
-            T_i = T_i * (1.0 - alpha_final + 1e-10)
+            T_i = T_i * (1.0 - alpha_final + 1e-7)
 
         filename = os.path.join(savedir, 'slow-mo_%03d.jpg'%(i))
         rgb8 = to8b(final_rgb.permute(1, 2, 0).cpu().numpy())
@@ -271,7 +271,7 @@ def render_lockcam_slowmo(ref_c2w, num_img,
             alpha_2_fianl = (1.0 - (1. - splat_alpha_dy_2) * (1. - splat_alpha_rig_2) ) * ratio
             alpha_final = alpha_1_final + alpha_2_fianl
 
-            T_i = T_i * (1.0 - alpha_final + 1e-10)
+            T_i = T_i * (1.0 - alpha_final + 1e-7)
 
         filename = os.path.join(savedir, '%03d.jpg'%(i))
         rgb8 = to8b(final_rgb.permute(1, 2, 0).cpu().numpy())
@@ -641,8 +641,8 @@ def create_nerf(args):
     """Instantiate NeRF's MLP model.
     """
     if args.nerf_model == 'PyTorch':
-        model = DynamicNeRF(args.use_fp16).to(device)
-        model_rigid = StaticNeRF(args.use_fp16).to(device)
+        model = DynamicNeRF().to(device)
+        model_rigid = StaticNeRF().to(device)
     elif args.nerf_model == 'CutlassMLP':
         model = CutlassDynamicNeRF().to(device)
         model_rigid = CutlassStaticNeRF().to(device)
@@ -747,7 +747,7 @@ def raw2outputs_blending(raw_dy,
     alpha_rig = (1. - torch.exp(-opacity_rigid * dists)) * (1. - raw_blend_w)
 
     Ts = torch.cumprod(torch.cat([torch.ones((alpha_dy.shape[0], 1)), 
-                                (1. - alpha_dy) * (1. - alpha_rig)  + 1e-10], -1), -1)[:, :-1]
+                                (1. - alpha_dy) * (1. - alpha_rig)  + 1e-7], -1), -1)[:, :-1]
     
     weights_dy = Ts * alpha_dy
     weights_rig = Ts * alpha_rig
@@ -762,7 +762,7 @@ def raw2outputs_blending(raw_dy,
     # compute dynamic depth only
     alpha_fg = 1. - torch.exp(-opacity_dy * dists)
     weights_fg = alpha_fg * torch.cumprod(torch.cat([torch.ones((alpha_fg.shape[0], 1)), 
-                                                                1.-alpha_fg + 1e-10], -1), -1)[:, :-1]
+                                                                1.-alpha_fg + 1e-7], -1), -1)[:, :-1]
     depth_map_fg = torch.sum(weights_fg * z_vals, -1)
     rgb_map_fg = torch.sum(weights_fg[..., None] * rgb_dy, -2) 
 
@@ -792,7 +792,7 @@ def raw2outputs_warp(raw_p,
 
     alpha = 1. - torch.exp(-opacity * dists)
 
-    weights = alpha * torch.cumprod(torch.cat([torch.ones((alpha.shape[0], 1)), 1.-alpha + 1e-10], -1), -1)[:, :-1]
+    weights = alpha * torch.cumprod(torch.cat([torch.ones((alpha.shape[0], 1)), 1.-alpha + 1e-7], -1), -1)[:, :-1]
     rgb_map = torch.sum(weights[...,None] * rgb, -2)  # [N_rays, 3]
 
     depth_map = torch.sum(weights * z_vals, -1)
@@ -828,12 +828,12 @@ def raw2outputs(raw, z_vals, rays_d, raw_noise_std=0, white_bkgd=False, pytest=F
         noise = torch.randn(raw[...,3].shape) * raw_noise_std
 
     alpha = raw2alpha(raw[...,3] + noise, dists)  # [N_rays, N_samples]
-    # weights = alpha * tf.math.cumprod(1.-alpha + 1e-10, -1, exclusive=True)
-    weights = alpha * torch.cumprod(torch.cat([torch.ones((alpha.shape[0], 1)), 1.-alpha + 1e-10], -1), -1)[:, :-1]
+    # weights = alpha * tf.math.cumprod(1.-alpha + 1e-7, -1, exclusive=True)
+    weights = alpha * torch.cumprod(torch.cat([torch.ones((alpha.shape[0], 1)), 1.-alpha + 1e-7], -1), -1)[:, :-1]
     rgb_map = torch.sum(weights[...,None] * rgb, -2)  # [N_rays, 3]
 
     depth_map = torch.sum(weights * z_vals, -1)
-    disp_map = 1./torch.max(1e-10 * torch.ones_like(depth_map), depth_map / torch.sum(weights, -1))
+    disp_map = 1./torch.max(1e-7 * torch.ones_like(depth_map), depth_map / torch.sum(weights, -1))
 
 
     return rgb_map, weights, depth_map