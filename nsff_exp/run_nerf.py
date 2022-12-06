More precision fixes and auto-loss scaling (#22)
* Fixed additional precision issues in compute_depth_loss, compute_mae, and NDC2Euclidean

* Adding layers to FusedMLP, since I can't get hash encoding to work. More layers improves training stability significantly, drops FPS from 16 to 14.5 - good trade-off

* Small increase in Fused MLP size. Added option for auto loss scaling

nsff_exp/run_nerf.py
@@ -171,6 +171,8 @@ def config_parser():
                         help='Default of tf.float16 for half-precision floating point training')
     parser.add_argument("--use_amp", action='store_true',
                         help='Use automated mixed-precision')
+    parser.add_argument("--use_loss_autoscaler", action='store_true',
+                        help='Use loss auto-scaler')
     parser.add_argument("--enable_fused_adam", action='store_true',
                         help='Enable fused kernel for Adam optimization - default False')
     parser.add_argument("--enable_pinned_memory", action='store_true',
@@ -366,9 +368,11 @@ def train():
     # Note: bfloat16 has the same range as float32, at the cost of precision.
     if args.use_amp:
         autocast_context = torch.autocast(device_type="cuda", dtype=torch.bfloat16)
-        loss_scaler = torch.cuda.amp.GradScaler()
     else:
         autocast_context = contextlib.nullcontext()
+
+    if args.use_loss_autoscaler:
+        loss_scaler = torch.cuda.amp.GradScaler()
 
     start = start + 1
     for i in trange(start, N_iters, mininterval=mininterval, maxinterval=maxinterval):
@@ -419,54 +423,53 @@ def train():
                     flow_fwd = flow_fwd + uv_grid
                     flow_bwd = flow_bwd + uv_grid
 
-                if N_rand is not None:
-                    with nvtx.annotate("Get Rays"):
-                        rays_o, rays_d = get_rays(H, W, focal, torch.Tensor(pose))  # (H, W, 3), (H, W, 3)
-                        coords = torch.stack(torch.meshgrid(torch.linspace(0, H-1, H), torch.linspace(0, W-1, W)), -1)  # (H, W, 2)
-                        coords = torch.reshape(coords, [-1,2])  # (H * W, 2)
+                with nvtx.annotate("Get Rays"):
+                    rays_o, rays_d = get_rays(H, W, focal, torch.Tensor(pose))  # (H, W, 3), (H, W, 3)
+                    coords = torch.stack(torch.meshgrid(torch.linspace(0, H-1, H), torch.linspace(0, W-1, W)), -1)  # (H, W, 2)
+                    coords = torch.reshape(coords, [-1,2])  # (H * W, 2)
 
-                        if args.use_motion_mask and i < decay_iteration * 1000:
-                            num_extra_sample = args.num_extra_sample
-                            select_inds_hard = np.random.choice(hard_coords.shape[0], 
-                                                                size=[min(hard_coords.shape[0], 
-                                                                    num_extra_sample)], 
-                                                                replace=False)  # (N_rand,)
-                            select_inds_all = np.random.choice(coords.shape[0], 
-                                                            size=[N_rand], 
+                    if args.use_motion_mask and i < decay_iteration * 1000:
+                        num_extra_sample = args.num_extra_sample
+                        select_inds_hard = np.random.choice(hard_coords.shape[0], 
+                                                            size=[min(hard_coords.shape[0], 
+                                                                num_extra_sample)], 
                                                             replace=False)  # (N_rand,)
-
-                            select_coords_hard = hard_coords[select_inds_hard].long()
-                            select_coords_all = coords[select_inds_all].long()
-
-                            select_coords = torch.cat([select_coords_all, select_coords_hard], 0)
-
-                        else:
-                            select_inds = np.random.choice(coords.shape[0], 
+                        select_inds_all = np.random.choice(coords.shape[0], 
                                                         size=[N_rand], 
                                                         replace=False)  # (N_rand,)
-                            select_coords = coords[select_inds].long()  # (N_rand, 2)
-                        
-                        rays_o = rays_o[select_coords[:, 0], 
+
+                        select_coords_hard = hard_coords[select_inds_hard].long()
+                        select_coords_all = coords[select_inds_all].long()
+
+                        select_coords = torch.cat([select_coords_all, select_coords_hard], 0)
+
+                    else:
+                        select_inds = np.random.choice(coords.shape[0], 
+                                                    size=[N_rand], 
+                                                    replace=False)  # (N_rand,)
+                        select_coords = coords[select_inds].long()  # (N_rand, 2)
+                    
+                    rays_o = rays_o[select_coords[:, 0], 
+                                    select_coords[:, 1]]  # (N_rand, 3)
+                    rays_d = rays_d[select_coords[:, 0], 
+                                    select_coords[:, 1]]  # (N_rand, 3)
+                    batch_rays = torch.stack([rays_o, rays_d], 0)
+                    target_rgb = target[select_coords[:, 0], 
                                         select_coords[:, 1]]  # (N_rand, 3)
-                        rays_d = rays_d[select_coords[:, 0], 
-                                        select_coords[:, 1]]  # (N_rand, 3)
-                        batch_rays = torch.stack([rays_o, rays_d], 0)
-                        target_rgb = target[select_coords[:, 0], 
-                                            select_coords[:, 1]]  # (N_rand, 3)
-                        target_depth = depth_gt[select_coords[:, 0], 
+                    target_depth = depth_gt[select_coords[:, 0], 
+                                        select_coords[:, 1]]
+                    target_mask = mask_gt[select_coords[:, 0], 
+                                        select_coords[:, 1]].unsqueeze(-1)
+
+                    target_of_fwd = flow_fwd[select_coords[:, 0], 
                                             select_coords[:, 1]]
-                        target_mask = mask_gt[select_coords[:, 0], 
-                                            select_coords[:, 1]].unsqueeze(-1)
+                    target_fwd_mask = fwd_mask[select_coords[:, 0], 
+                                            select_coords[:, 1]].unsqueeze(-1)#.repeat(1, 2)
 
-                        target_of_fwd = flow_fwd[select_coords[:, 0], 
-                                                select_coords[:, 1]]
-                        target_fwd_mask = fwd_mask[select_coords[:, 0], 
-                                                select_coords[:, 1]].unsqueeze(-1)#.repeat(1, 2)
-
-                        target_of_bwd = flow_bwd[select_coords[:, 0], 
-                                                select_coords[:, 1]]
-                        target_bwd_mask = bwd_mask[select_coords[:, 0], 
-                                                select_coords[:, 1]].unsqueeze(-1)#.repeat(1, 2)
+                    target_of_bwd = flow_bwd[select_coords[:, 0], 
+                                            select_coords[:, 1]]
+                    target_bwd_mask = bwd_mask[select_coords[:, 0], 
+                                            select_coords[:, 1]].unsqueeze(-1)#.repeat(1, 2)
 
                 img_idx_embed = img_i/num_img * 2. - 1.0
 
@@ -614,14 +617,13 @@ def train():
                     depth_loss + entropy_loss
 
             optimizer.zero_grad()
-            if args.use_amp:
+            if args.use_loss_autoscaler:
                 loss_scaler.scale(loss).backward()
                 loss_scaler.step(optimizer)
                 loss_scaler.update()
             else:
                 loss.backward()
                 optimizer.step()
-
 
             # NOTE: IMPORTANT!
             ###   update learning rate   ###

nsff_exp/run_nerf_helpers.py
@@ -57,7 +57,10 @@ def ndc_rays(H, W, focal, near, rays_o, rays_d):
     return rays_o, rays_d
 
 
-def compute_depth_loss(pred_depth, gt_depth):   
+def compute_depth_loss(pred_depth, gt_depth):
+    pred_depth = pred_depth.to(torch.float32)
+    gt_depth = gt_depth.to(torch.float32)
+
     t_pred = torch.median(pred_depth)
     s_pred = torch.mean(torch.abs(pred_depth - t_pred))
 
@@ -79,7 +82,9 @@ def compute_mse(pred, gt, mask, dim=2):
         mask_rep = mask.repeat(1, 1, pred.size(-1))
 
     num_pix = torch.sum(mask_rep.to(torch.float32)) + 1e-7
-    return torch.sum( (pred - gt)**2 * mask_rep )/ num_pix
+    scaled_mask = mask_rep.to(torch.float32) / num_pix
+
+    return torch.sum((pred - gt)**2 * scaled_mask)
 
 def compute_mae(pred, gt, mask, dim=2):
     if dim == 1:
@@ -90,8 +95,9 @@ def compute_mae(pred, gt, mask, dim=2):
         mask_rep = mask.repeat(1, 1, pred.size(-1))
 
     num_pix = torch.sum(mask_rep.to(torch.float32)) + 1e-7
+    scaled_mask = mask_rep.to(torch.float32) / num_pix
 
-    return torch.sum( torch.abs(pred - gt) * mask_rep )/ num_pix
+    return torch.sum(torch.abs(pred - gt) * scaled_mask)
 
 
 def normalize_depth(depth):
@@ -263,6 +269,7 @@ def perspective_projection(pts_3d, h, w, f):
     return pts_2d    
 
 
+
 def se3_transform_points(pts_ref, raw_rot_ref2prev, raw_trans_ref2prev):
     pts_prev = torch.squeeze(torch.matmul(raw_rot_ref2prev, pts_ref[..., :3].unsqueeze(-1)) + raw_trans_ref2prev)
     return pts_prev
@@ -276,13 +283,12 @@ def NDC2Euclidean(xyz_ndc, H, W, f):
     #   z_e = 2./ (xyz_ndc[..., 2:3] - 1. + 1e-6)
     #
     # Clamp instead...
-    #
-    z_e = 2./ (torch.clamp(xyz_ndc[..., 2:3], -1.0 + 1e-3, 1.0 - 1e-3) - 1.0)
+    z_e = 2./ (torch.clamp(xyz_ndc[..., 2:3], -1.0, 0.99) - 1.0)
     x_e = - xyz_ndc[..., 0:1] * z_e * W/ (2. * f)
     y_e = - xyz_ndc[..., 1:2] * z_e * H/ (2. * f)
 
     xyz_e = torch.cat([x_e, y_e, z_e], -1)
- 
+
     return xyz_e
 
 import sys